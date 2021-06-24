2ef1260c4bcfc814fb4c0a926900f2dbda3aac81
kyleashtonm <55991082+kyleashtonm@users.noreply.github.com>
2021-06-23 23:17:42 -0400

    Update Problem Set 1.py

--- Unit 2/Problem Set 1.py
+++ Unit 2/Problem Set 1.py
@@ -1,3 +1,4 @@
+# Problem 1 - Paying Debt off in a Year
 # Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
